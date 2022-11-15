1f4369af4f7ff12ddf8aaa2c70bfcda0d65b0cc3
市川恭佑 <github@mail.ebi-yade.com>
2022-11-15 14:30:28 +0900

    Create and cleanup a set of a dataset and a table

--- main.go
+++ main.go
@@ -17,7 +17,24 @@ import (
 
 const (
 	projectID = "test-project"
+	datasetID = "test_dataset"
+	tableID   = "test_table"
 	timeout   = 300
+)
+
+var (
+	// For information about nested schema, see below
+	// https://cloud.google.com/bigquery/docs/samples/bigquery-nested-repeated-schema
+	schema = bigquery.Schema{
+		{
+			Name:     "labels",
+			Required: false,
+			Type:     bigquery.RecordFieldType,
+			Schema: bigquery.Schema{
+				{Name: "log_id", Required: false, Type: bigquery.StringFieldType},
+			},
+		},
+	}
 )
 
 func main() {
@@ -37,7 +54,7 @@ func run(ctx context.Context) error {
 	if addr == "" {
 		return errors.New("error BIGQUERY_EMULATOR_HOST must not be empty")
 	}
-	// 参考: Spanner SDK がエミュレータのアドレス切り替えに使っている部分
+	// The following option imitates the one of Spanner emulator.
 	// https://github.com/googleapis/google-cloud-go/blob/5b307584fcd635635aae6a2fc4ba8252f2bbe22d/spanner/client.go#L177-L186
 	opts := []option.ClientOption{
 		option.WithEndpoint(addr),
@@ -50,5 +67,36 @@ func run(ctx context.Context) error {
 		return errors.Wrap(err, "error NewClient")
 	}
 	log.Printf("[DEBUG] client created: %#v\n", cli)
+
+	// Creating a dataset and a table
+	dataset := cli.Dataset(datasetID)
+	defer clean(dataset)
+	if err := dataset.Create(ctx, nil); err != nil {
+		return errors.Wrap(err, "error Create dataset")
+	}
+	dsMeta, err := dataset.Metadata(ctx)
+	if err != nil {
+		return errors.Wrap(err, "error dataset.Metadata")
+	}
+	log.Printf("[DEBUG] dataset created: %#v\n", dsMeta)
+	table := dataset.Table(tableID)
+	if err := table.Create(ctx, &bigquery.TableMetadata{
+		Schema: schema,
+	}); err != nil {
+		return errors.Wrap(err, "error Create table")
+	}
+	tbMeta, err := table.Metadata(ctx)
+	if err != nil {
+		return errors.Wrap(err, "error table.Metadata")
+	}
+	log.Printf("[DEBUG] table created: %#v\n", tbMeta)
 	return nil
+}
+
+func clean(dataset *bigquery.Dataset) {
+	if err := dataset.DeleteWithContents(context.TODO()); err != nil {
+		log.Printf("[ERROR] error dataset.DeleteWithContents: %s\n", err)
+		return
+	}
+	log.Printf("[DEBUG] dataset is deleted")
 }
